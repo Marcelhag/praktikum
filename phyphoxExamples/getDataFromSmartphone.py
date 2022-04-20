b081e7bef3a40d9a275c2f6a71199dcfbd105e2f
Marcelhag <marcel.hagedorn@gmail.com>
2022-04-20 12:58:11 +0200

    init

---
+++ phyphoxExamples/getDataFromSmartphone.py
@@ -0,0 +1,58 @@
+from phyphoxBLE import PhyphoxBLE, Experiment
+import machine
+import time
+
+p = PhyphoxBLE()
+lastTimestamp = 0
+blinkInterval = 1000
+led = True
+ledPin = 22
+p.debug = False
+
+def main():
+    global lastTimestamp
+    global led
+    p.start("My Device")
+    p._write_callback = receivedData
+    buildInLed = machine.Pin(ledPin, machine.Pin.OUT)    
+    
+    #Experiment
+    getDataFromSmartphone = Experiment()   #generate experiment on Arduino which plot random values
+    getDataFromSmartphone.setTitle("Set Blink Interval")
+    getDataFromSmartphone.setCategory("Micropython Experiments")
+    getDataFromSmartphone.setDescription("User can set Blink Interval of Mikrocontroller LED")    
+
+    #View
+    firstView = Experiment.View()
+    firstView.setLabel("FirstView") #Create a "view"
+
+    #Edit
+    Interval = Experiment.Edit() 
+    Interval.setLabel("Interval")
+    Interval.setUnit("ms")
+    Interval.setSigned(False)
+    Interval.setDecimal(False)
+    Interval.setChannel(1)
+
+    firstView.addElement(Interval)
+    getDataFromSmartphone.addView(firstView)
+    p.addExperiment(getDataFromSmartphone)
+
+    while True:
+        if time.ticks_ms()-lastTimestamp > blinkInterval:
+            lastTimestamp = time.ticks_ms();
+            led = not led;
+            buildInLed.value(led)
+
+def receivedData():          # get data from PhyPhox app
+    global blinkInterval
+    receivedInterval = p.read()
+    if receivedInterval > 0 and receivedInterval != blinkInterval:
+        print("New Interval: ", receivedInterval)
+        blinkInterval = receivedInterval
+
+
+if __name__ == "__main__":
+    main()
+
+
